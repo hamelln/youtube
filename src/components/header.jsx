8aec20dd6a5b8ce810439e906e6f1c3b6bf5fce0
hamelln <39308313+hamelln@users.noreply.github.com>
2021-03-22 15:17:31 +0900

    search

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,13 +4,18 @@ class Header extends Component {
     render() {
         return (
             <div className="masthead-container">
-                <div className="ytd-container">Youtube</div>
+                <div className="ytd-container">
+                <img src={process.env.PUBLIC_URL + '/images/logo.png'} /> 
+                Youtube
+                </div>
                 <div className="search-container">
                     <div className="yt-searchbox">
                         <input id="search" type="text" placeholder="검색"/>
                     </div>
                     <button id="search-icon-legacy.ytd-searchbox">
-                    <yt-icon className="search-icon">icon-svg</yt-icon>
+                    <yt-icon className="search-icon">
+                    <img src={process.env.PUBLIC_URL + '/images/search.png'} /> 
+                    </yt-icon>
                     </button>
                 </div>
             </div>
